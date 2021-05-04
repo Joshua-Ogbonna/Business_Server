e13ace6eced06d6684802574c2eee00581535605
Joshua Ogbonna <devjaykes@gmail.com>
2021-05-04 16:18:18 +0100

    Modified cross origin

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const PORT = process.env.PORT || 3040
+const PORT = process.env.PORT || 3040;
 
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cors())
-app.use(cookieParser())
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/auth', require('./routes/invoice'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", require("./routes/invoice"));
 
-mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-    console.log(`Database successfully connected on ${process.env.MONGODB_URI}`)
-})
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => {
+    console.log(
+      `Database successfully connected on ${process.env.MONGODB_URI}`
+    );
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`Server running on PORT ${3040}`)
-})
+  console.log(`Server running on PORT ${3040}`);
+});
